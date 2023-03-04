f69702331bb4409f1bc2efed08e7360b4b9869e4
Fabio Henrique <Farabio973@hotmail.com>
2023-03-04 19:09:15 -0300

    Segunda versão com estilo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ function Vencedor($tabuleiro, $jogador)
     || $tabuleiro[0][0] == $jogador && $tabuleiro[1][1] == $jogador && $tabuleiro[2][2] == $jogador
     || $tabuleiro[0][2] == $jogador && $tabuleiro[1][1] == $jogador && $tabuleiro[2][0] == $jogador
     ){
-        return "O ganhador é $jogador";
+        return " O vencedor é \"$jogador\"";
     }
 }
 
@@ -28,6 +28,7 @@ $tabuleiro = array(
 $jogador1 = "o";
 $jogador2 = "x";
 $vencedor = NULL;
+$aviso = NULL;
 
 if(isset($_POST['acao'])){
     $tabuleiro = array(
@@ -44,7 +45,7 @@ if(isset($_POST['acao'])){
             $vencedor = Verificar($tabuleiro, $jogador1, $jogador2);
             
     }else{
-        echo "Deve usar x ou o";
+        $aviso = "Apenas X e O";
     }
 }
 
@@ -61,6 +62,7 @@ if(isset($_POST['acao'])){
 </head>
 <body>
     <form method="post">
+        <p><?php echo $aviso;?></p>
         <div class="p1">
             <input type="text" name="a1" maxlength="1">
             <input type="text" name="a2" maxlength="1">
@@ -78,11 +80,15 @@ if(isset($_POST['acao'])){
         </div>
         <input type="submit" name="acao" value="Verificar">
     </form>
-    <p>Resultado do jogo: <b><?php echo $vencedor;?></b></p>
-    <p><?php if(isset($_POST['acao'])){
-                echo $tabuleiro[0][0],$tabuleiro[0][1],$tabuleiro[0][2]."<br>";
-                echo $tabuleiro[1][0],$tabuleiro[1][1],$tabuleiro[1][2]."<br>";
-                echo $tabuleiro[2][0],$tabuleiro[2][1],$tabuleiro[2][2];}
-    ?></p>
+    <div class="resultado">
+        <p>Resultado do jogo: <b><?php echo $vencedor;?></b></p>
+        <div class="a1">
+            <p><?php if(isset($_POST['acao'])){
+                        echo $tabuleiro[0][0]."|".$tabuleiro[0][1]."|".$tabuleiro[0][2]."<br>";
+                        echo $tabuleiro[1][0]."|".$tabuleiro[1][1]."|".$tabuleiro[1][2]."<br>";
+                        echo $tabuleiro[2][0]."|".$tabuleiro[2][1]."|".$tabuleiro[2][2];}
+            ?></p>
+        </div>
+    </div>
 </body>
 </html>
